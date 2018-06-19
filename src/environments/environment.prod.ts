6164b2ad763cfea9ca05cd519606aae24d64d5aa
Marcus Wilson <mswilson4040@hotmail.com>
2018-06-18 20:42:12 -0700

    fix environment url path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api: {
-    path: 'https://marcswilson-api.azurewebsites.net/api/mlb/boxscores'
+    path: 'https://marcswilson-api.azurewebsites.net/api'
   }
 };
